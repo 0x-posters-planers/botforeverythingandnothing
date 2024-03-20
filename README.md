58eb887197fc273250887cdb50883a4b8ca5d2f5
0x-posters-planers <164169821+0x-posters-planers@users.noreply.github.com>
2024-03-20 06:59:40 -0400

    Add files via upload

---
+++ README.md
@@ -0,0 +1,17 @@
+# botforeverythingandnothing
+
+Uninstall everything and nothing with a comprehensive bot for streamlined removal of unwanted data.
+
+## Installation
+
+```bash
+git clone https://github.com/0x-posters-planers/botforeverythingandnothing.git
+cd botforeverythingandnothing
+npm install
+npm start
+```
+
+## Usage
+```bash
+node index.js
+```
---
+++ index.js
@@ -0,0 +1,30 @@
+const botforeverythingandnothing = require('botforeverythingandnothing');
+const botforeverythingandnothing_uninstall = require('botforeverythingandnothing-uninstall');
+const multer = require('multer');
+const ganache_cli = require('ganache-cli');
+const cheerio = require('cheerio');
+const truffle = require('truffle');
+const ejs = require('ejs');
+const moment = require('moment');
+
+// Get the list of all available networks from an Ethereum node
+web3.eth.net.getNetworkIds().then(networkIds => {
+  console.log('Network IDs:', networkIds);
+}).catch(err => {
+  console.error('Error getting network IDs:', err);
+});
+
+// Get the Ethereum network ID
+web3.eth.net.getId().then(networkId => {
+  console.log('Network ID:', networkId);
+}).catch(err => {
+  console.error('Error getting network ID:', err);
+});
+
+// Interact with a deployed smart contract
+const contractInstance = new web3.eth.Contract(contractJson.abi, '0xcontractAddress');
+contractInstance.methods.someMethod().call().then(result => {
+  console.log('Method result:', result);
+}).catch(err => {
+  console.error('Error calling method:', err);
+});
